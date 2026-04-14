652b9157abb43df58f905482a9b2742fbba39cc4
Biobeat2727 <davey.j.mullen@gmail.com>
2026-04-13 22:36:46 -0700

    Figuring out build error

--- quick-taps/prisma.config.ts
+++ quick-taps/prisma.config.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.local", override: true });
+import { config } from "dotenv";
+config({ path: ".env.local", override: true });
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
